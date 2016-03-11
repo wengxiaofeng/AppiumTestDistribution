4f2284c323b20625060bcd881ffb0afeaad20dbb
Sai Krishna <saikrishna321@yahoo.com>
2016-03-11 14:52:01 +0530

    handled escaspe quote

--- src/main/java/com/appium/manager/CucumberRunner.java
+++ src/main/java/com/appium/manager/CucumberRunner.java
@@ -12,12 +12,13 @@ public class CucumberRunner {
 
     CommandPrompt commandPrompt = new CommandPrompt();
     Process p ;
+
     public void triggerParallelCukes(String feature) throws IOException, InterruptedException {
     	System.out.println("Distribution triggered");
     	System.out.println(System.getProperty("user.dir") + "/libs/cucumber-core-1.2.4.jar:");
         String classpPath= System.getProperty("user.dir") + "/target/dependency/*:";
-        System.out.println(classpPath);
-        String a = "java -cp " + "\'" + classpPath + "\'"+
+        System.out.println("\"classpPath\"");
+        String a = "java -cp \"" + classpPath +"\"" +
                 System.getProperty("user.dir") + "/target/classes:" +
                 System.getProperty("user.dir") + "/target/test-classes" +
                 " cucumber.api.cli.Main " +
@@ -25,8 +26,12 @@ public class CucumberRunner {
                 System.getProperty("user.dir") + "/src/test/java/com/cucumber/features/" + feature +
                 " --plugin json:" + System.getProperty("user.dir") + "/target/" + feature + ".json";
         System.out.println(a);
-        commandPrompt.runCommand(a);
-        Thread.sleep(2000);
+        p = Runtime.getRuntime().exec(a);
+        /*String[] a1 = new String[]{"java", "-cp \"" + classpPath +"\"", System.getProperty("user.dir") + "/target/classes:", System.getProperty("user.dir") + "/target/test-classes"
+                , "cucumber.api.cli.Main", "--glue com.test.steps", System.getProperty("user.dir") + "/src/test/java/com/cucumber/features/" + feature, "--plugin json:", System.getProperty("user.dir") + "/target/" + feature + ".json"};
+        p = Runtime.getRuntime().exec(new String[]{"java", "-cp \"" + classpPath +"\"", System.getProperty("user.dir") + "/target/classes:", System.getProperty("user.dir") + "/target/test-classes"
+        , "cucumber.api.cli.Main", "--glue com.test.steps", System.getProperty("user.dir") + "/src/test/java/com/cucumber/features/" + feature, "--plugin json:", System.getProperty("user.dir") + "/target/" + feature + ".json"  });
+        */Thread.sleep(2000);
 
     }
 
