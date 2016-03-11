6f588ebd253cb84b74a6bc4bc78e37ccfb486c93
Sai Krishna <saikrishna321@yahoo.com>
2016-03-11 17:06:49 +0530

    aaded method to get process details

--- src/main/java/com/appium/manager/CucumberRunner.java
+++ src/main/java/com/appium/manager/CucumberRunner.java
@@ -2,7 +2,9 @@ package com.appium.manager;
 
 import com.appium.utils.CommandPrompt;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 
 /**
@@ -11,14 +13,17 @@ import java.io.IOException;
 public class CucumberRunner {
 
     CommandPrompt commandPrompt = new CommandPrompt();
-    Process p ;
+    Process p;
 
     public void triggerParallelCukes(String feature) throws IOException, InterruptedException {
-    	System.out.println("Distribution triggered");
-    	System.out.println(System.getProperty("user.dir") + "/libs/cucumber-core-1.2.4.jar:");
-        String classpPath= System.getProperty("user.dir") + "/target/dependency/*:";
-        System.out.println("\"classpPath\"");
-        String a = "java -cp \"" + classpPath +"\"" +
+        System.out.println("Distribution triggered");
+        System.out.println(System.getProperty("user.dir") + "/libs/cucumber-core-1.2.4.jar:");
+        String compileClasses=System.getProperty("user.dir") + "/target/classes:";
+        String compileTestClasses=System.getProperty("user.dir") + "/target/test-classes";
+        String classpPath = System.getProperty("user.dir") + "/target/dependency/*:";
+        String reportPath= System.getProperty("user.dir") + "/target/" + feature + ".json";
+        System.out.println("" + classpPath + "");
+        String a = "java -cp \"" + classpPath + "\"" +
                 System.getProperty("user.dir") + "/target/classes:" +
                 System.getProperty("user.dir") + "/target/test-classes" +
                 " cucumber.api.cli.Main " +
@@ -26,17 +31,42 @@ public class CucumberRunner {
                 System.getProperty("user.dir") + "/src/test/java/com/cucumber/features/" + feature +
                 " --plugin json:" + System.getProperty("user.dir") + "/target/" + feature + ".json";
         System.out.println(a);
-        p = Runtime.getRuntime().exec(a);
-        /*String[] a1 = new String[]{"java", "-cp \"" + classpPath +"\"", System.getProperty("user.dir") + "/target/classes:", System.getProperty("user.dir") + "/target/test-classes"
-                , "cucumber.api.cli.Main", "--glue com.test.steps", System.getProperty("user.dir") + "/src/test/java/com/cucumber/features/" + feature, "--plugin json:", System.getProperty("user.dir") + "/target/" + feature + ".json"};
-        p = Runtime.getRuntime().exec(new String[]{"java", "-cp \"" + classpPath +"\"", System.getProperty("user.dir") + "/target/classes:", System.getProperty("user.dir") + "/target/test-classes"
-        , "cucumber.api.cli.Main", "--glue com.test.steps", System.getProperty("user.dir") + "/src/test/java/com/cucumber/features/" + feature, "--plugin json:", System.getProperty("user.dir") + "/target/" + feature + ".json"  });
-        */Thread.sleep(2000);
 
+        String[] a1 = new String[]{ "java", "-cp", classpPath,compileClasses, compileTestClasses, "cucumber.api.cli.Main","--glue com.test.steps",
+                System.getProperty("user.dir") + "/src/test/java/com/cucumber/features/" + feature,
+                "--plugin json:", reportPath};
+        Thread.sleep(2000);
+
+        final String[] command = a1;
+        try {
+            p = Runtime.getRuntime().exec(command);
+        } catch (final IOException e) {
+            e.printStackTrace();
+        }
+
+        //Wait to get exit value
+        try {
+            p.waitFor();
+            final int exitValue = p.waitFor();
+            if (exitValue == 0)
+                System.out.println("Successfully executed the command: " + command);
+            else {
+                System.out.println("Failed to execute the following command: " + command + " due to the following error(s):");
+                try (final BufferedReader b = new BufferedReader(new InputStreamReader(p.getErrorStream()))) {
+                    String line;
+                    if ((line = b.readLine()) != null)
+                        System.out.println(line);
+                } catch (final IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 
-
-/*    @Test
+/*
+  @Test
     public void testApp() throws IOException, InterruptedException {
         triggerParallelCukes("Basket.feature");
     }*/
